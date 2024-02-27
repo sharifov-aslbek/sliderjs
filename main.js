35d38875a6b276b48ea644b4148f5096f733ddae
sharifov-aslbek <sharifovaslbek5@gmail.com>
2024-02-27 17:42:19 +0500

    addded

--- main.js
+++ main.js
@@ -1,24 +1,26 @@
 let forImg = document.querySelectorAll(".slider .content img")
 let forNextBtn = document.querySelector(".slider .next")
 let forPrevBtn = document.querySelector(".slider .prev")
-
+let callingLi = document.querySelectorAll(".slider .btns .dots .dot")
 let counter = 0
-
-
-
-
   
 // let index = 0
 
 forNextBtn.addEventListener("click" , nextBtn)
 function nextBtn () {
     forImg[counter].classList.remove("active")
+    dots[counter].classList.remove("active")
     counter++
     if (counter === forImg.length) {
         counter = 0
     }
-    
+    dots[counter].classList.add("active")
     forImg[counter].classList.add("active")  
+
+
+    // callingLi[counter].classList.remove("active")
+    // counter++
+    // callingLi[counter].classList.add("active")
     
 }
 
@@ -28,10 +30,12 @@ forPrevBtn.addEventListener("click" , prevBtn)
 
 function prevBtn () {
     forImg[counter].classList.remove("active")
+    dots[counter].classList.remove("active")
     if (counter === 0) {
         counter = forImg.length
     }
     counter--
+    dots[counter].classList.add("active")
     forImg[counter].classList.add("active")
 }
 
